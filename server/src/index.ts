066ae5dccb880597d64deaf4259abe5878664688
Phat Tran <phat.tranminh@employmenthero.com>
2023-03-05 23:05:02 +0700

    Switch to socket.io

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,39 @@
 import express from 'express'
 import http from 'http'
-import ws from 'ws'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 4000
 const app = express()
 const server = http.createServer(app)
-const wsServer = new ws.Server({ server })
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+})
 
-wsServer.on("connection", ws => {
-  ws.on("message", msg => {
+type Room = {
+  [key: string]: any
+}
+
+const rooms: Room = {}
+
+io.on("connection", socket => {
+  socket.on("message", msg => {
     console.log("Received message: %s from client", msg);
   });
   //send immediate a feedback to the incoming connection
-  ws.send(
+  socket.send(
     JSON.stringify({
       type: "connect",
       message: "Well hello there, I am a WebSocket server"
     })
   );
+
+  socket.on("create room", roomId => {
+    rooms[roomId] = [socket.id]
+    console.log("Created room with ID: " + roomId)
+    console.log("Total rooms: " + Object.keys(rooms).length)
+  })
 });
 
 server.listen(PORT, () => console.log(`Signaling server started on port ${PORT}`))
